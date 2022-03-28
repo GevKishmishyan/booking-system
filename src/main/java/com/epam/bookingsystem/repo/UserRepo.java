c4770c73aa10ed9b69264e35aba13624d6c38325
Aram <aram050385@gmail.com>
2022-03-28 14:59:05 +0400

    done swagger.New version

--- src/main/java/com/epam/bookingsystem/repo/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.epam.bookingsystem.repo;
-
-import com.epam.bookingsystem.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository <User, Integer> {
-
-}
