69392903c2da4e91d7ae6bd1447870c4b84be14e
Aram <aram050385@gmail.com>
2022-03-28 14:56:57 +0400

    done swagger.New version

--- src/main/java/com/epam/bookingsystem/BookingSystemApplication.java
+++ src/main/java/com/epam/bookingsystem/BookingSystemApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookingSystemApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(BookingSystemApplication.class, args);
     }
 
 }
+
