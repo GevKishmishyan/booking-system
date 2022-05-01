1ed5d9f00f48a571f0c3626c5010092c1402b452
GevKishmishyan <kishmishyan.gevorg@gmail.com>
2022-05-01 15:27:33 +0400

    change method name

--- src/main/java/com/epam/bookingsystem/http/ResponseBuilder.java
+++ src/main/java/com/epam/bookingsystem/http/ResponseBuilder.java
@@ -13,18 +13,18 @@ public class ResponseBuilder {
     public ResponseBuilder() {
     }
 
-    public  static <T, V> ResponseEntity<V> build(HttpStatus status, T body, Mapper<T, V> mapper){
-        return ResponseEntity.status(status).body(mapper.mapDTO(body));
+    public static <T, V> ResponseEntity<V> build(HttpStatus status, T body, Mapper<T, V> mapper) {
+        return ResponseEntity.status(status).body(mapper.mapToDto(body));
     }
 
-    public  static <T, V>ResponseEntity<Page<V>> build(HttpStatus status, Page<T> page, Mapper<T, V> mapper){
-        return ResponseEntity.status(status).body(page.map(mapper::mapDTO));
+    public static <T, V> ResponseEntity<Page<V>> build(HttpStatus status, Page<T> page, Mapper<T, V> mapper) {
+        return ResponseEntity.status(status).body(page.map(mapper::mapToDto));
     }
 
-    public static <T, V> ResponseEntity<List<V>> build(HttpStatus status, List<T> list, Mapper<T, V> mapper){
+    public static <T, V> ResponseEntity<List<V>> build(HttpStatus status, List<T> list, Mapper<T, V> mapper) {
         return ResponseEntity.status(status).body(
                 list.stream()
-                        .map(mapper::mapDTO)
+                        .map(mapper::mapToDto)
                         .collect(Collectors.toList())
         );
     }
--- src/main/java/com/epam/bookingsystem/mapper/Mapper.java
+++ src/main/java/com/epam/bookingsystem/mapper/Mapper.java
@@ -1,5 +1,5 @@
 package com.epam.bookingsystem.mapper;
 
 public interface Mapper<T,V>  {
-    V mapDTO(T t);
+    V mapToDto(T t);
 }
